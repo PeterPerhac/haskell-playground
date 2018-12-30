a1bfc050aee60c453c780bdd158536d89cb42569
Peter Perhac <perhac.peter@gmail.com>
2018-12-30 12:19:07 +0000

    unfoldr to generate fibonacci sequence

--- scratchpad/src/Lib.hs
+++ scratchpad/src/Lib.hs
@@ -1,6 +1,9 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (fibs) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List
+
+-- using `unfoldr` to generate a Fibonacci sequence
+fibs :: IO ()
+fibs = putStrLn . intercalate ", " . take 100 . map show $ unfoldr fibfu (1, 0)
+  where fibfu (n1, n2) = Just (n1 + n2, (n2, n1 + n2))
+
